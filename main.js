4dc43791c56f079d0f8c77e6bab885aafd274f77
unknown <cajkovskijpavel5@gmail.com>
2021-01-22 16:13:50 +0100

    koniec js

---
+++ main.js
@@ -0,0 +1,35 @@
+// es 5 
+var imionaPracownikowZ_Firmy = "Pawel"
+// es 6
+
+const imionaPracownikowZ_Firmy2 = "Kamil"
+
+const numByScring = "2"
+
+const NumString = "10"
+const num = 10
+const num0 = 1
+const num1 = 1
+const prawda = true // to 1 jako liczba
+const falsz = false // to zero jako liczba
+
+console.log(num0 >= num1)
+
+console.log(num0 !== num1)
+console.log(!prawda)
+
+const arr = ["Janusz", 56, true, num0]
+
+const obj = {
+    name: "Janusz", 
+    age: 13,
+    adress: "Nowa 15"
+}
+console.log(obj.age)
+
+const respons = [
+    { name: "Dawid", age: 13 },
+    { name: "Jakub", age: 17 }
+]
+
+console.log(respons[1].name)
